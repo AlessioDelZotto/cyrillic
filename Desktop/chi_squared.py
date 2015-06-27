9725bbdff5c3c73f37ef75dc9d885fc083df2773
Alessio Del Zotto <alessio.delzotto@gmail.com>
2015-06-27 19:08:48 +0200

    Create chi_squared.py

---
+++ Desktop/chi_squared.py
@@ -0,0 +1,15 @@
+from scipy import stats
+import matplotlib.pyplot as plt
+import pandas as pd
+import collections
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
+plt.show()
+
+chi, p = stats.chisquare(freq.values())
