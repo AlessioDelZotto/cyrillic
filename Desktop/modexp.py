fd48ac2bc05a4e19bde51f314bf097473d81efe4
Alessio Del Zotto <alessio.delzotto@gmail.com>
2015-07-11 18:26:54 +0200

    commit message

--- Desktop/modexp.py
+++
@@ -1,9 +0,0 @@
-nexp = input("Choose a number n: ")
-exp2 = 2**nexp
-modexp = exp2 % 9
-print "If we multiply 2 n times, we get: "
-print exp2,
-print "\nIf we divide this number by 9 we get the following rest: "
-print modexp;
-
-
